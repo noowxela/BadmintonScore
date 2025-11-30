02c9a95945adc879e0e213f49f81bc7ad1104256
Alex Woon Jun ROng <noowxela@gmail.com>
2025-11-30 17:24:15 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/badminton_board">
+    <BrowserRouter basename="/BadmintonScore">
       <App />
     </BrowserRouter>
   </StrictMode>,
